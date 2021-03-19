57cba79d901146784e895e1bd0edeaf0590c6d43
Timothy Pham <tpham2580@gmail.com>
2021-03-19 15:44:26 -0500

    User persistence and added post page

--- app/src/main/java/com/example/instaclone/ParseApplication.java
+++ app/src/main/java/com/example/instaclone/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.instaclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
             .applicationId("v1dQBTjU1pls5v6iqPdG2F1p7wWxeAgCjd8Uozsk")
